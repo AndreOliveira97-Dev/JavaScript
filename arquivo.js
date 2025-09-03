c1dbaad2c389b46c410c7b5ed24c12f9515dc99b
AndreOliveira07-Dev <andresaile07@gmail.com>
2025-09-02 23:31:09 -0300

    atualização do projeto

--- arquivo.js
+++ arquivo.js
@@ -1,1 +1,23 @@
+const convertButton = document.querySelector(".convert-button")
+
+function buttonClicked (){
+    const enteredValue = document.querySelector(".entered-value").value
+    const dollarValue = 5.47
+    const valueResult = enteredValue / dollarValue
+    const realValue = document.querySelector(".real-value")
+    const result = document.querySelector(".result")
+
+    realValue.innerHTML = new Intl.NumberFormat ("pt-BR", {
+        style: "currency",
+        currency: "BRL" 
+    }).format(enteredValue)
+    result.innerHTML = new Intl.NumberFormat ("en-US", {
+        style: "currency",
+        currency: "USD" 
+    }).format(valueResult)
+
+}
+
+convertButton.addEventListener("click", buttonClicked)
+
 
